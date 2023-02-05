d0a0b062669e25e68e68b38e72280f854231d95f
Oleksandr Okhrimovich <ohrimovich.sanek@gmail.com>
2023-02-05 22:29:48 +0200

    upd(*) add fonts and images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import logo from './images/logo.png';
 import './App.css';
+import Container from './components/Container/Container';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Container>
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1>Марія Янчук</h1>
+					<p>
+          Психолог
+					</p>
+				</header>
+			</Container>
+
+		</div>
+	);
 }
 
 export default App;
