caccd4a7fce77dc5a51f9ca21c208dacbae27472
Oleksandr Okhrimovich <ohrimovich.sanek@gmail.com>
2023-02-22 00:11:17 +0200

    upd(*) add animation to hero

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Circles from './components/Circles/Circles';
+import Container from './components/Container/Container';
 
 function App() {
 	return (
 		<div className="App">
+			<Container>
 				<Circles/>
 				<Header />
 				<Hero />
+				</Container>
 		</div>
 	);
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,6 @@ import dark_logo from '../../images/header-logo.png';
 import light_logo from '../../images/footer-logo.png';
 import { ReactComponent as MenuIcon } from '../../images/icons/menu-icon.svg';
 import { ReactComponent as CloseIcon } from '../../images/icons/close-icon.svg'
-import Container from '../Container/Container';
 
 import { useState } from 'react';
 
@@ -34,27 +33,25 @@ const Header = () => {
 	return (
 
 		<header>
-			<Container>
-				<div className={style.wrapper}>
-					<a href="/"><img src={isOpen ? light_logo : dark_logo} alt="Logo" /></a>
-					<nav className={isOpen ? style.active : null}>
-						<ul className={style.nav_list}>
-							{links.map(link => {
-								return (
-									<li key={link.link} className={style.nav_item}><a className={style.nav_link} href={link.link}>{link.value}</a></li>
-								)
-							})}
-							<li className={style.nav_item}> <a className={style.consultation} href="#consultation">Безкоштовна консультація</a></li>
-						</ul>
-					</nav>
-					<div className={style.tablet_buttons}>
-						{isOpen ? null : <a href='#consultation' className={style.consultation}>Безкоштовна консультація</a>}
-						<button type='button' onClick={() => setIsOpen(!isOpen)} className={style.menu_btn}>
-							{isOpen ? <CloseIcon /> : <MenuIcon />}
-						</button>
-					</div>
+			<div className={style.wrapper}>
+				<a href="/"><img src={isOpen ? light_logo : dark_logo} alt="Logo" /></a>
+				<nav className={isOpen ? style.active : null}>
+					<ul className={style.nav_list}>
+						{links.map(link => {
+							return (
+								<li key={link.link} className={style.nav_item}><a className={style.nav_link} href={link.link}>{link.value}</a></li>
+							)
+						})}
+						<li className={style.nav_item}> <a className={style.consultation} href="#consultation">Безкоштовна консультація</a></li>
+					</ul>
+				</nav>
+				<div className={style.tablet_buttons}>
+					{isOpen ? null : <a href='#consultation' className={style.consultation}>Безкоштовна консультація</a>}
+					<button type='button' onClick={() => setIsOpen(!isOpen)} className={style.menu_btn}>
+						{isOpen ? <CloseIcon /> : <MenuIcon />}
+					</button>
 				</div>
-			</Container>
+			</div>
 		</header >
 
 
