2622d6ecf9031bfaa180f077218cb114ef6e92a6
Oleksandr Okhrimovich <ohrimovich.sanek@gmail.com>
2023-03-29 11:59:59 +0300

    fix(*) fix styles

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -12,17 +12,18 @@ const Contacts = () => {
     <section id='contacts' className={style.contacts}>
       <Container>
         <div className={style.wrapper}>
-          <img src={MariaWithClock} alt="Психолог Марія" />
-          <div className={style.content_wrapper}>
-            <h2 className={style.title}>Контакти</h2>
-            <p>Буду рада бачити Вас на консультаціях Я на зв'язку у будні дні з 10 до 19 години </p>
-            <h3 className={style.subtitle}>Графік роботи</h3>
-            <ul>
-              <li><ClockIcon className={ style.icon} />Пн-Пт: 10:00-19:00, Сб-Нд: вихідні</li>
-              <li><EnvelopIcon className={ style.icon}/><a href="mailto:info@mariiayanchuk.com.ua">info@mariiayanchuk.com.ua</a></li>
-              <li><PhoneIcon className={ style.icon}/><a href="tel:+380976945250">+38 (097) 69 452 50</a></li>
-            </ul>
-
+          <div className={style.content}>
+            <img src={MariaWithClock} alt="Психолог Марія" />
+            <div className={style.content_wrapper}>
+              <h2 className={style.title}>Контакти</h2>
+              <p>Буду рада бачити Вас на консультаціях Я на зв'язку у будні дні з 10 до 19 години </p>
+              <h3 className={style.subtitle}>Графік роботи</h3>
+              <ul>
+                <li><ClockIcon className={ style.icon} />Пн-Пт: 10:00-19:00, Сб-Нд: вихідні</li>
+                <li><EnvelopIcon className={ style.icon}/><a href="mailto:info@mariiayanchuk.com.ua">info@mariiayanchuk.com.ua</a></li>
+                <li><PhoneIcon className={ style.icon}/><a href="tel:+380976945250">+38 (097) 69 452 50</a></li>
+              </ul>
+            </div>
           </div>
         </div>
       </Container>
